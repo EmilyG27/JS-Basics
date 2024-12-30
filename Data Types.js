2f7814d3d0f72e93287b906323c29fc273ba3503
EmilyG27 <emilylgrinstead@gmail.com>
2024-12-29 23:57:08 -0600

    first commit

---
+++ Data Types.js
@@ -0,0 +1,9 @@
+let studentName = "Luna";
+let studentAge = 11;
+let studentGrade = 100;
+let isPresent = true;
+
+console.log(`Name: ${studentName}`);
+console.log(`Age: ${studentAge}`);
+console.log(`Grade: ${studentGrade}`);
+console.log(`Present: ${isPresent ? "yes" : "no"}`);
